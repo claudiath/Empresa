86e339fd8afc76249aeb917d3b4de8861426f687
Claudia Torres Hernández <claudiatorreshernandez88@gmail.com>
2021-02-25 21:01:44 +0100

    Agregar archivos de proyecto.

---
+++ Empresa/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Empresa.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
